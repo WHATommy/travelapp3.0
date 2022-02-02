19740b74abaac95b3d998e913fc3f3306de5ed30
Tommy Tran <tranman100598@gmail.com>
2022-02-02 16:14:40 -0700

    Created sign up api to register users
    Created signup.js in the api folder
    
    added jsonwebtoken, bcryptjs, and express-validator
    
    Implemented the POST request router to register users into the system
    
    Created the route connection in the server.js with "api/signup"

--- models/UserModel.js
+++ models/UserModel.js
@@ -27,6 +27,6 @@ const UserSchema = new Schema({
     }
 });
 
-const User = mongoose.model('users',UserSchema);
+const User = mongoose.model('users', UserSchema);
 
-module.exports = { User };
+module.exports =  User ;
