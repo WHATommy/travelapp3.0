18f559acc0db8310c797471072fa2f417e48c241
Tommy Tran <tranman100598@gmail.com>
2022-02-01 23:18:29 -0700

    Create trip schema (#4)
    * Created User Model
    
    added UserModel.js in model folder
    
    * Updated UserModel.js
    
    Added properties to make them username and email unique, and password to not be part of any query
    
    Renamed date to createdAt
    
    Changed 'string' to String
    Changed 'array' to Array
    
    * Create Trip Model
    
    Added TripModel.js into model folder

---
+++ models/TripModel.js
@@ -0,0 +1,183 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+    trip: {
+        type: Schema.Types.ObjectId,
+        ref: 'trips'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: String
+    },
+    endDate: {
+        type: String
+    },
+    checkInTime: {
+        type: String
+    },
+    checkOutTime: {
+        type: String
+    },
+    cost: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    phoneNumber: {
+        type: String
+    },
+    websiteUrl: {
+        type: String
+    }
+});
+
+const RestaurantSchema = new Schema({
+    trip: {
+        type: Schema.Types.ObjectId,
+        ref: 'trips'
+    },
+    user: {
+        type: String
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number
+    },
+    date: {
+        type: Date
+    },
+    checkInTime: {
+        type: String
+    },
+    checkOutTime: {
+        type: String
+    },
+    cost: {
+        type: String
+    },
+    categories: {
+        type: Array
+    },
+    phoneNumber: {
+        type: String
+    },
+    websiteUrl: {
+        type: String
+    },
+    img_url: {
+        type: Array
+    }
+});
+
+const HousingSchema = new Schema({
+    trip: {
+        type: Schema.Types.ObjectId,
+        ref: 'trips'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date
+    },
+    endDate: {
+        type: Date
+    },
+    checkInTime: {
+        type: String
+    },
+    checkOutTime: {
+        type: String
+    },
+    cost: {
+        type: Number
+    },
+    websiteUrl: {
+        type: String
+    }, 
+    attendees: {
+        type: Array
+    }
+});
+
+const AttendeeSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    name: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    moderator: {
+        type: Boolean
+    },
+    attending: {
+        type: Boolean
+    }
+});
+
+const TripSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String
+    },
+    startDate: {
+        type: Date
+    },
+    endDate: {
+        type: Date
+    },
+    startTime: {
+        type: String
+    },
+    endTime: {
+        type: String
+    },
+    eventList: {
+        type: [EventSchema]
+    },
+    restaurantList: {
+        type: [RestaurantSchema]
+    },
+    housingList: {
+        type: [HousingSchema]
+    },
+    attendeeList: {
+        type: [AttendeeSchema]
+    }
+});
+
+const Trip = mongoose.model('trips', TripSchema);
+
+module.exports = { Trip };
---
+++ models/UserModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true, 
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now   
+    },
+    trips: {
+        type: Array
+    }
+});
+
+const User = mongoose.model('users',UserSchema);
+
+module.exports = { User };
