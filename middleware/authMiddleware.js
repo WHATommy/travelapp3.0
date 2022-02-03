a041554ea06020eb2962dc96c64e237ca44bb160
Tommy Tran <tranman100598@gmail.com>
2022-02-03 09:15:10 -0700

    Create auth middleware (#10)
    * Auth api to log user in
    
    Created auth.js in api folder
    
    User can login into the database and return a token to use
    
    Routed the auth api in server.js
    
    * Create authMiddleware.js
    
    Added authMiddleware.js in middleware folder
    
    Developers can now use authMiddleware to make api routes exclusive to authorized users

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header("auth-token");
+
+  // Check if token exist
+  if (!token) {
+    return res.status(401).json({ msg: "Access denied" });
+  }
+
+  // Verify the token
+  try {
+    jwt.verify(token, process.env.jwtSecret, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: "Invalid token" });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+};
