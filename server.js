b6f3e25cd1395c5bc26f2b940c47ddca0f850257
Tommy Tran <tranman100598@gmail.com>
2022-02-15 00:44:24 -0700

    Connect backend and frontend (#32)
    * Connect backend and frontend
    
    imported handle from nextjs to link pages with server.js
    
    * remove components folder

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 require("dotenv").config({ path: "./config.env" });
 const connectDb = require("./utilsServer/connectDb");
 connectDb();
